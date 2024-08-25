3a73f66e970ceb90d222d560affd526d71bcabbf
Javier Rivera <javierivera29@gmail.com>
2024-08-25 07:01:39 -0600

    Agregado estilos y validación de inputs vacíos

--- app.js
+++ app.js
@@ -1,59 +1,76 @@
-const { ref, createApp, computed } = Vue;
+const { ref, createApp, computed, watch } = Vue;
 
 createApp({
-    setup() {
-        const listaFrases = ref([
-            {
-                frase: "El código es poesía",
-                autor: "Automattic"
-            },
-            {
-                frase: "Un error es una oportunidad para aprender",
-                autor: "Robert C. Martin"
-            },
-            {
-                frase: "La programación es como la cocina: sigue la receta, pero también improvisa",
-                autor: "John Romero"
-            },
-            {
-                frase: "El código limpio es el código que se mantiene limpio",
-                autor: "Martin Fowler"
-            },
-            {
-                frase: "No reinventes la rueda; reutiliza el código",
-                autor: "Solomon Hykes"
-            },
-        ])
+  setup() {
+    const listaFrases = ref([
+      {
+        frase: "El código es poesía",
+        autor: "Automattic"
+      },
+      {
+        frase: "Un error es una oportunidad para aprender",
+        autor: "Robert C. Martin"
+      },
+      {
+        frase: "La programación es como la cocina: sigue la receta, pero también improvisa",
+        autor: "John Romero"
+      },
+      {
+        frase: "El código limpio es el código que se mantiene limpio",
+        autor: "Martin Fowler"
+      },
+      {
+        frase: "No reinventes la rueda; reutiliza el código",
+        autor: "Solomon Hykes"
+      },
+    ])
 
-        const nuevaFrase = ref('');
-        const nuevoAutor = ref('');
+    const nuevaFrase = ref('');
+    const nuevoAutor = ref('');
+    const mostrarMensajeError = ref(false);
+    const mensajeError = computed(() => {
+      if (!nuevoAutor.value && !nuevaFrase.value) return "Complete todos los campos";
+      if (!nuevaFrase.value) return "Ingrese una frase";
+      if (!nuevoAutor.value) return "Ingrese el autor";
+    })
 
-        const agregarFrase = () => {
-            if (!nuevaFrase.value.trim() || !nuevoAutor.value.trim()) {
-                return null
-            }
-            listaFrases.value.unshift(
-                {
-                    frase: nuevaFrase.value,
-                    autor: nuevoAutor.value,
-                }
-            );
-            nuevaFrase.value = '';
-            nuevoAutor.value = '';
-            return;
+    const watcher = watch([nuevaFrase, nuevoAutor], () => {
+      if (mostrarMensajeError) {
+        if (nuevaFrase.value || nuevoAutor.value) {
+          mostrarMensajeError.value = false;
         }
+      }
+    });
 
-        const eliminarFrase = (index) => {
-            const confirmarEliminar = confirm("Eliminar frase?")
-            confirmarEliminar ? listaFrases.value.splice(index, 1) : null
-        }
-
-        return ({
-            listaFrases,
-            nuevaFrase,
-            nuevoAutor,
-            agregarFrase,
-            eliminarFrase,
-        })
+    const agregarFrase = () => {
+      if (!nuevaFrase.value.trim() || !nuevoAutor.value.trim()) {
+        mostrarMensajeError.value = true;
+        return;
+      }
+      listaFrases.value.unshift({ frase: nuevaFrase.value, autor: nuevoAutor.value });
+      nuevaFrase.value = '';
+      nuevoAutor.value = '';
+      return;
     }
+
+    const eliminarFrase = (index) => {
+      return listaFrases.value.splice(index, 1)
+    }
+
+    const editarFrase = () => {
+
+    }
+
+    return ({
+      listaFrases,
+      nuevaFrase,
+      nuevoAutor,
+      mostrarMensajeError,
+      mensajeError,
+      watcher,
+      agregarFrase,
+      eliminarFrase,
+      editarFrase,
+    })
+  },
 }).mount("#app")
